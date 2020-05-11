4c9573c0bfc17c7746d24abbc09e0cf662b3ae7f
Sergey Kozub <kozubsa@gmail.com>
2020-05-12 00:49:05 +0300

    web server

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module hello-world
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/google/uuid v1.1.1
+)
--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	"log"
+	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
+	"time"
 )
 
 func main() {
-	log.Println("hello world")
-	sigStop := make(chan os.Signal, 1)
-	signal.Notify(sigStop, syscall.SIGINT, syscall.SIGTERM)
-	<-sigStop
+	router := gin.New()
+	router.Use(gin.Recovery())
+
+	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+		return fmt.Sprintf("[%s] - %s %s path=%s, status_code=%d, latency=%s, user_agent=%s, error_message=%s\n",
+			param.TimeStamp.Format(time.RFC1123),
+			param.ClientIP,
+			param.Method,
+			param.Path,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
+
+	router.GET("/", func(c *gin.Context) {
+		text := fmt.Sprintf("%s - %s",os.Getenv("TEST_TEXT"), uuid.New().String())
+		c.String(http.StatusOK, text)
+	})
+
+	log.Printf("start web server on %s", os.Getenv("PORT"))
+
+	if err := router.Run(":" + os.Getenv("PORT")); err != nil {
+		log.Println("router run :"+os.Getenv("PORT"), err)
+	}
 }
