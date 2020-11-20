b748cf592b1303864da14cd652d63374a8faefd5
Sergey Kozub <kozubsa@gmail.com>
2020-11-20 21:48:23 +0300

    fix

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
 module hello-world
 
-go 1.14
+go 1.15
 
 require (
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/google/uuid v1.1.1
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/google/uuid"
 	"log"
 	"net/http"
@@ -10,7 +11,16 @@ import (
 	"time"
 )
 
+type Service struct {
+	TelegramBotApi    *tgbotapi.BotAPI
+}
+
 func main() {
+	s := &Service{}
+	s.TelegramBotApi = getTelegramBotApiConn()
+
+ go	s.GetMessageFromTelegram()
+
 	router := gin.New()
 	router.Use(gin.Recovery())
 
@@ -27,14 +37,85 @@ func main() {
 		)
 	}))
 
-	router.GET("/", func(c *gin.Context) {
-		text := fmt.Sprintf("%s - %s",os.Getenv("TEST_TEXT"), uuid.New().String())
-		c.String(http.StatusOK, text)
-	})
+	router.GET("/", s.Test)
 
 	log.Printf("start web server on %s", os.Getenv("PORT"))
 
 	if err := router.Run(":" + os.Getenv("PORT")); err != nil {
 		log.Println("router run :"+os.Getenv("PORT"), err)
 	}
+}
+
+func (s *Service) GetMessageFromTelegram() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := s.TelegramBotApi.GetUpdatesChan(u)
+	if err != nil {
+		log.Println(err)
+	}
+
+	for update := range updates {
+		// check Message, group, user
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+		if update.Message.Chat == nil {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		if _, err := s.TelegramBotApi.Send(msg); err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (s *Service) Test(c *gin.Context) {
+	text := fmt.Sprintf("%s - %s", os.Getenv("TEST_TEXT"), uuid.New().String())
+
+	files := []string{
+		"AgACAgIAAxkBAAIHT1-1qFEYD4ZrVnhRZVKZjGNFwne9AAIEsTEbMOKwSbh5zGI2DbgssSJzly4AAwEAAwIAA20AA9hCAwABHgQ",
+		"AgACAgIAAxkBAAIHT1-1qFEYD4ZrVnhRZVKZjGNFwne9AAIEsTEbMOKwSbh5zGI2DbgssSJzly4AAwEAAwIAA3gAA9dCAwABHgQ",
+	}
+
+	publishDate := time.Now().UTC()
+	messageText := fmt.Sprintf("``` %s ``` %s", publishDate.Format("01.02.2006 15:04:05 MST"), "Post number 8 | group 2 Adult")
+	var medias []interface{}
+	for i, file := range files {
+		f := tgbotapi.NewInputMediaPhoto(file)
+
+		if i == 0 {
+			f.ParseMode = "markdown"
+			f.Caption = messageText
+		}
+
+		medias = append(medias, f)
+	}
+
+	msg := tgbotapi.NewMediaGroup(-494747213, medias)
+
+	if _, err := s.TelegramBotApi.Send(msg); err != nil {
+		log.Println(err)
+	}
+
+	c.String(http.StatusOK, text)
+}
+
+func getTelegramBotApiConn() *tgbotapi.BotAPI {
+	bot := &tgbotapi.BotAPI{}
+	var err error
+	for {
+		bot, err = tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_BOT_SECRET"))
+		if err != nil {
+			log.Println("new bot api connection error:", err)
+			continue
+		}
+
+		bot.Debug = false
+		break
+	}
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	return bot
 }
