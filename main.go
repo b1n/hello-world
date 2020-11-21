cd1359ae8f72fa083b6e6cd529901c7dd0dc8861
Sergey Kozub <kozubsa@gmail.com>
2020-11-21 12:49:51 +0300

    fix

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 
 	go s.GetMessageFromTelegram()
 
-	msg := tgbotapi.NewMessage(-494747213, "service started")
+	msg := tgbotapi.NewMessage(-494747213, "service started 2")
 	if _, err := s.TelegramBotApi.Send(msg); err != nil {
 		log.Println(err)
 	}
